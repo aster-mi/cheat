5c864533962ca750591a52088c5c8ddc9ccf19f1
aster-mi <aster.micro.829@gamil.com>
2021-08-10 02:28:16 +0900

    モーダルを作成

--- src/main/java/com/local/cheat/model/Cheat.java
+++ src/main/java/com/local/cheat/model/Cheat.java
@@ -1,5 +1,6 @@
 package com.local.cheat.model;
 
+
 import com.local.cheat.constants.URL;
 import com.local.cheat.util.EscapeUtil;
 
@@ -7,6 +8,9 @@ import lombok.Data;
 
 @Data
 public class Cheat {
+	
+	/** コードをデフォルトで表示する最大行数 */
+	private final Integer CODE_MAX_LINE = 10;
 
 	private Integer id;
 	private String title;
@@ -15,6 +19,21 @@ public class Cheat {
 	
 	public String getEscapeDetail() {
 		return EscapeUtil.htmlEscape(detail).replaceAll(URL.LINK_REGEX,"<a href='$0'>$0</a>");
+	}
+	
+	public boolean isCodeOverflow() {
+		var n = System.lineSeparator();
+		return code.split(n,-1).length > CODE_MAX_LINE;
+	}
+	
+	public String getShortCode() {
+		var n = System.lineSeparator();
+		var lines = code.split(n,-1);
+		String shortCode = "";
+		for(int i=0;lines.length>i&&CODE_MAX_LINE>i;i++) {
+			shortCode += lines[i]+n;
+		}
+		return shortCode;
 	}
 	
 }
